2158b14dcc5960a5819dff5a787bfcf6eee03388
Calamity Jane <calamity_jane@macbook-air-de-calamity.home>
2023-05-02 17:55:54 +0200

    daemon

--- daemon.py
+++ daemon.py
@@ -1,2 +1,14 @@
 def daemon(numbers: list[int], k: int) -> bool:
-    ...
+    """
+    Determine if numbers is partitioned array and if item at k index is a pivot :
+    1. All items before k index or stricly inferior to item at k index
+    2. All items after k index or superior or equal to item at k index
+    Solution must have a max O(n) complexity with n len of numbers
+    """
+    for number in numbers[0:k]:
+        if number >= numbers[k]:
+            return False
+    for number in numbers[k:]:
+        if number < numbers[k]:
+            return False
+    return True
