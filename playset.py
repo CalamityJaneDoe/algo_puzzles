9bece748cc35f6989f87a3d2f125bd30b2c0e64f
Calamity Jane <calamity_jane@macbook-air-de-calamity.home>
2023-05-03 17:06:55 +0200

    code improvment

--- playset.py
+++ playset.py
@@ -1,14 +1,8 @@
-import string
-
-
 def playset(s: str) -> bool:
     """
     Returns True if there is one or several character(s) duplicate(s) within str
     Solution must have a O(n) complexity in time with n len of str
     """
-    alphabet_dict = dict.fromkeys(string.ascii_lowercase, 0)
+    str_set = set(s)
 
-    for char in s:
-        alphabet_dict[char] += 1
-
-    return 2 in alphabet_dict.values()
+    return len(s) != len(str_set)
